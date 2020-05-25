0e6ac0535ead4c257d9c4ab92e2d13793437e3b6
Luke <luke@cloudthrifty.com>
2020-05-25 13:08:53 -0400

    init

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/goog-lukemc/tserver"
+)
+
+func main() {
+	var port string
+	var ok bool
+	if port, ok = os.LookupEnv("PORT"); !ok {
+		port = "8080"
+	}
+
+	port = fmt.Sprintf(":%s", port)
+	server := tserver.NewServer(&tserver.ServerConfig{
+		Addr: port,
+	})
+	server.Start(tserver.DefaultHandlers)
+}
